72380e34c4c833714e4f957b56b9bebd05889f38
Ozcan Sert <ozcsert0@gmail.com>
2025-01-02 21:28:23 +0300

    update: folder structure

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,40 +1,16 @@
-import { useState, useRef } from "react"
+import { useState } from "react"
 import { useParams } from "react-router-dom"
-import { VideoPlayer } from "../../components/VideoContainer/VideoPlayer"
-import { useContext, useEffect } from "react"
-import { RoomContext } from "../../context/roomContext"
+
 import "./styles.scss"
 import avatar from "@/public/Avatar.svg"
 import { AppBar } from "@/components/AppBar"
 import { VideoContainer } from "@/components/VideoContainer"
 import { WhiteBoard } from "@/components/WhiteBoard"
 import { ToolBar } from "@/components/ToolBar"
-// SAYFA YENILENDIGINDE IKI ADET GELIYOR BIRI WIDGET OLARAK DIGERI DEFAULT VIDEO OLARAK
-type PeerState = Record<string, { stream: MediaStream }>
 
-type Props = {
-  id: string | undefined
-}
 export const Room = () => {
   const { id } = useParams()
   const [userIcon, setUserIcon] = useState<string[]>(["1", "2"])
-  // const { ws, me, stream, peers, userName } = useContext(RoomContext)
-
-  // useEffect(() => {
-  //   if (me) {
-  //     // if (!stream) return
-
-  //     const { _id, _username } = me
-
-  //     const userData = {
-  //       _id,
-  //       _username,
-  //     }
-
-  //     ws.emit("join-room", { roomID: id, userData: userData })
-  //     console.log("join room emitted")
-  //   }
-  // }, [id, me, ws])
 
   return (
     <>
@@ -54,11 +30,6 @@ export const Room = () => {
         </div>
         <div className="room__video-stack-container">
           <VideoContainer id={id} />
-          {/* <VideoPlayer stream={stream} />
-
-          {Object.values(peers as PeerState).map((peer, index) => (
-            <VideoPlayer key={index} stream={peer.stream} />
-          ))} */}
         </div>
         <div>
           <ToolBar />
